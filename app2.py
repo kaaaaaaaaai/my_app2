8c37c0390c55a813d9611bfbbb969998a717714c
kaaaaaaaaai <123119768+kaaaaaaaaai@users.noreply.github.com>
2023-01-20 00:30:57 +0000

    1st

--- app2.py
+++ app2.py
@@ -2,15 +2,19 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_line(m, b):
+def plot_func(a, b, c):
     x = np.linspace(-10, 10, 100)
-    y = m*x + b
+    y = a*x**2 + b*x + c
     plt.plot(x, y)
     plt.show()
 
-st.title("Linear Function Plotter")
-m = st.slider("m", -10.0, 10.0, 0.5)
-b = st.slider("b", -10.0, 10.0, 0)
+st.title("Function Plotter")
+
+a = st.sidebar.slider("a", -10.0, 10.0, 1.0)
+b = st.sidebar.slider("b", -10.0, 10.0, 2.0)
+c = st.sidebar.slider("c", -10.0, 10.0, 1.0)
+
+st.write("y = a*x^2 + b*x + c")
 
 if st.button("Plot"):
-    plot_line(m, b)
+    plot_func(a, b, c)
